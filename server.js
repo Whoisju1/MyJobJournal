d69fa529dfa4bb4c1238efb57b0c8419a9451c88
Juan <whoisju1@gmail.com>
2017-08-06 08:14:03 -0400

    user mongo-connect to prevent sessions from droping upon server restart

--- server.js
+++ server.js
@@ -4,26 +4,36 @@ const keys = require('./config/keys');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 require('./models/User');
 require('./services/passport');
 const app = express();
 
+
 // connect mongoose to database
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
+
+// user express sessions as a middleware
+
+// app.use(session({
+//     store: new MongoStore({ mongooseConnection: mongoose.connection })
+// }));
 
 app.use(
 	session({
 		secret: keys.cookieKey,
 		maxAge: 30 * 24 * 60 * 60 * 1000,
 		resave: false,
-		saveUninitialized: false
+		saveUninitialized: false,
+		store: new MongoStore({ mongooseConnection: mongoose.connection })
 	})
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(bodyParser.json());
 // import route handlers
 require('./routes/index')(app);
 
