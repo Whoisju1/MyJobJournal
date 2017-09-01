c2009d16669b72f1ab290456f4e0715d8d007c86
Juan <whoisju1@gmail.com>
2017-08-31 21:06:34 -0400

    remove prod.js from .gitignore

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleSecreteID: process.env.GOOGLE_SECRETE_ID,
+    cookieKey:process.env.COOKIE_KEY
+};
