1e285042eab7950b070f120d74367d108f3e30d8
Juan <whoisju1@gmail.com>
2017-08-31 21:55:50 -0400

    move production optimization down some lines

--- server.js
+++ server.js
@@ -26,16 +26,6 @@ mongoose.connect(keys.mongoURI);
 // });
 
 // user express sessions as a middleware
-if (process.env.NODE_ENV === 'production') {
-    //express will server up production assets like the main.js file or main.css file
-    app.use(express.static('client/build'));
-    
-    //express will serve up the index.html file if it doesn't recognize the route
-    const path = require('path');
-    app.get('*', (req, res) => {
-       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
-    });
-}
 
 app.use(
 	session({
@@ -53,6 +43,17 @@ app.use(passport.session());
 app.use(bodyParser.json());
 // import route handlers
 require('./routes/index')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    //express will server up production assets like the main.js file or main.css file
+    app.use(express.static('client/build'));
+    
+    //express will serve up the index.html file if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
