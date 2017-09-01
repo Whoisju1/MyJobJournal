28d964bfb6c314ee5c3acfe50be3cdae94ab0fff
Juan <whoisju1@gmail.com>
2017-08-31 21:00:31 -0400

    set other video as background for backup

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,6 +4,8 @@ export const Landing = props => {
 	return (
 		<video className="video-source" autoPlay loop>
 			<source className="video-source" src={require('./../videos/MP4/In-And-Out.mp4')} />
+			<source className="video-source" src={require('./../videos/OGV/In-And-Out.ogv')} />
+			<source className="video-source" src={require('./../videos/WEBM/In-And-Out.webm')} />
 		</video>
 	);
 };
