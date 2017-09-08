2d4a455640ea479bef17aaa9c93e6e35bb296b08
Juan <whoisju1@gmail.com>
2017-09-07 21:08:01 -0400

    create aside component

---
+++ client/src/components/Aside.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Aside = (props) => {
+    return(
+        <aside className="aside">
+            {this.props.children}
+        </aside>
+    )
+}
+
+export default Aside;
