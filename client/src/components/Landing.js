2ea5f17939f37e5cabebb649bf1d091785944bcd
Juan <whoisju1@gmail.com>
2017-09-16 13:07:39 -0400

    fix how page renders upon loading depending on authentication status

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import AuthButtons from './AuthButtons';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import Overview from './Overview';
 
 export const Landing = props => {
+	const renderContent = () => {
+		switch (props.auth) {
+			case null:
+				return;
+			case false:
+				return <AuthButtons />;
+			default:
+				return <Overview />;
+		}
+	};
+
 	return (
 		<div>
-			
-			{props.auth ? <Overview /> : <AuthButtons /> }
+			{renderContent()}
 			<video className="video-source" autoPlay loop>
 				<source className="video-source" src={require('./../videos/MP4/In-And-Out.mp4')} />
 				<source className="video-source" src={require('./../videos/OGV/In-And-Out.ogv')} />
@@ -18,10 +28,10 @@ export const Landing = props => {
 	);
 };
 
-const mapStateToProps = ({auth}) => {
+const mapStateToProps = ({ auth }) => {
 	return {
 		auth
-	}
-}
+	};
+};
 
 export default connect(mapStateToProps)(Landing);
