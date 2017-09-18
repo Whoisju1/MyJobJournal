2e62464cba98eedd6bf949615213ab2fbd2eb1f5
Juan <whoisju1@gmail.com>
2017-09-18 00:08:03 -0400

    make items returned in serarch results links

--- client/src/components/searchResults.js
+++ client/src/components/searchResults.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import {Link} from 'react-router-dom';
 
 class SearchResults extends React.Component {
 	constructor() {
@@ -18,21 +19,20 @@ class SearchResults extends React.Component {
 		const { searchTerm } = this.props.match.params;
 		const { search: results } = this.props;
 		if (!results) return;
-        console.log('length: ',results.length);
         if (results.length === 0) return <div>No results found for {searchTerm}</div>;
 		return results.map((item, i, array) => {
-                console.log('item: ', item);
-                console.log('index: ', i);
-                console.log('array: ', array);
 			return (
+                <Link to={`/application/${item._id}`}>
 				<div className='search-item' key={item._id}>
-                    {console.log('inside of return: ',item)}
-					Testing:
 					<p className={"search-company"}>
 						{`Company: ${item.company}`}
 					</p>
 					<p className={"search-position"}>{`Position: ${item.position}`}</p>
+                    <p className="search-status">
+                        {`Status: ${item.status}`}
+                    </p>
 				</div>
+                </Link>
 			);
 		});
 	}
