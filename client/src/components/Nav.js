5d294b011e7d99ee55bdc93f2cbaad7d5beca1a0
Juan <whoisju1@gmail.com>
2017-09-26 23:09:32 -0400

    show length of favs and applications

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,36 +1,46 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-export const Nav = (props) => {
+export const Nav = props => {
+	const showLength = () => {
+		if (props.data) return props.data.applications.length;
+	};
 
-    const showLength = () =>{ 
-        if(props.data) return props.data.applications.length;
-    };
-    
-    return(
-        <div>
-            <nav className="navbar">
-            <Link to="/applications">
-                <div className="application-link-wrapper">
-                        Dashboard
-                   
-                    <div className="application-numbers">
-                        {showLength() || 0}
-                    </div>
-                </div>
-            </Link>
-                <Link to="/favorites">
-                    Favorites
-                </Link>
-            </nav>
-        </div>
-    )
-}
+	const showFavLength = () => {
+		if (props.data) {
+			const { applications } = props.data;
+			const favorites = applications.filter(item => {
+				return item.favorite === true;
+			});
 
-const mapStateToProps = ({data}) => {
-    return {data}
-}
+			return favorites.length;
+		}
+	};
+
+	return (
+		<div>
+			<nav className="navbar">
+				<NavLink to="/applications" activeClass='nav-active'>
+					<div className="application-link-wrapper">
+						Dashboard
+						<div className="application-numbers">{showLength() || 0}</div>
+					</div>
+				</NavLink>
+				<NavLink to="/favorites" activeClass='nav-active'>
+					<div className="application-link-wrapper">
+						Favorites
+						<div className="fav-numbers">{showFavLength() || 0}</div>
+					</div>
+				</NavLink>
+			</nav>
+		</div>
+	);
+};
+
+const mapStateToProps = ({ data }) => {
+	return { data };
+};
 
 export default connect(mapStateToProps, actions)(Nav);
