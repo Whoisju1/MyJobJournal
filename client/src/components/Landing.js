bfb9b67cd4bda8924464f9b8fd1aea3c3d6c00f0
Juan John-Charles <whoisju1@gmail.com>
2018-01-10 14:12:34 -0500

    make video container for landing page a styled-components element

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 import AuthButtons from './AuthButtons';
 import Overview from './Overview';
 
 export const Landing = (props) => {
+  const VideoWrapper = styled.div`
+    height: calc(100vh - 45px);
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+  `;
+
   if (props.auth) props.history.push('/applications');
 
   const renderContent = () => {
@@ -19,14 +28,14 @@ export const Landing = (props) => {
   };
 
   return (
-    <div className="landing-page">
-      <video className="video-source" autoPlay loop>
-        <source className="video-source" src={require('./../videos/MP4/In-And-Out.mp4')} />
-        <source className="video-source" src={require('./../videos/OGV/In-And-Out.ogv')} />
-        <source className="video-source" src={require('./../videos/WEBM/In-And-Out.webm')} />
+    <VideoWrapper>
+      <video autoPlay loop>
+        <source src={require('./../videos/MP4/In-And-Out.mp4')} />
+        <source src={require('./../videos/OGV/In-And-Out.ogv')} />
+        <source src={require('./../videos/WEBM/In-And-Out.webm')} />
       </video>
       {renderContent()}
-    </div>
+    </VideoWrapper>
   );
 };
 
