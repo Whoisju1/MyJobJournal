41c2eb871079b3d8767b8a5ff101a1841a2a045b
Juan John-Charles <whoisju1@gmail.com>
2018-01-15 14:32:18 -0500

    import redux-form to form component

--- client/src/components/AppForm.js
+++ client/src/components/AppForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
