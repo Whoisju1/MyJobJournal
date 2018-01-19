ba5aaa19f559f4ef5cc3205034bc97f6b7a0b212
Juan John-Charles <whoisju1@gmail.com>
2018-01-19 12:50:48 -0500

    fix typo in update reducer

--- client/src/reducers/updateReducer.js
+++ client/src/reducers/updateReducer.js
@@ -1,11 +1,10 @@
-import {UPDATE_DATA} from '../actions/types';
+import { UPDATE_DATA } from '../actions/types';
 
-export default (state=null, actions) => {
-    switch (actions.types) {
-        case UPDATE_DATA:
-            
-        return actions.payload;
-        default:
-            return state;
-    }
+export default (state = null, action) => {
+  switch (action.type) {
+    case UPDATE_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
 };
