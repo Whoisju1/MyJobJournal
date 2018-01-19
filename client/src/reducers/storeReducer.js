9281b27aa3eea47b93250304f8f5a95b51400b14
Juan John-Charles <whoisju1@gmail.com>
2018-01-19 11:32:52 -0500

    clean up store reducer

--- client/src/reducers/storeReducer.js
+++ client/src/reducers/storeReducer.js
@@ -1,11 +1,10 @@
-import {STORE_DATA} from '../actions/types';
+import { STORE_DATA } from '../actions/types';
 
 export default function (state = null, action) {
-    switch (action.type) {
-        case STORE_DATA:
-            return action.payload;
-    
-        default:
-            return state;
-    }
-};
+  switch (action.type) {
+    case STORE_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
+}
