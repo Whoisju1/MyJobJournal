6a9336bfae560ec1e54c7b8bc43bef3959fa6f8a
Juan John-Charles <whoisju1@gmail.com>
2018-02-03 10:54:55 -0500

    wire up redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
   <Provider store={store}>
