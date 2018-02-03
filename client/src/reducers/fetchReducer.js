d74a3d03a241674fde9890bb9ecfa22350b35d69
Juan John-Charles <whoisju1@gmail.com>
2018-02-03 14:45:16 -0500

    clean up fetchData reducer to comply with eslint standards

--- client/src/reducers/fetchReducer.js
+++ client/src/reducers/fetchReducer.js
@@ -1,10 +1,10 @@
-import {FETCH_DATA} from '../actions/types';
+import { FETCH_DATA } from '../actions/types';
 
 export default function (state = null, action) {
-    switch (action.type) {
-        case FETCH_DATA:
-            return action.payload;
-        default:
-            return state;
-    }    
+  switch (action.type) {
+    case FETCH_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
 }
