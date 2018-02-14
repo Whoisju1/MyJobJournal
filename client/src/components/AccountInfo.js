d2124c09bcc3e9e39909137d27eb3db27798ffb0
Juan John-Charles <whoisju1@gmail.com>
2018-02-14 10:16:19 -0500

    empty out AccountInfo component

--- client/src/components/AccountInfo.js
+++ client/src/components/AccountInfo.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const AccountInfo = (...props) => (
-    <div className="account">
-        <h1 className="account__info">
-            Coming Soon (Under Construction)
-        </h1>
-    </div>
+const AccountInfo = () => (
+  <div>
+      Under Construction
+  </div>
 );
 
 export default AccountInfo;
