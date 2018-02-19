26ff041f3532cde1acb88e8725f8e2d320bf8d6f
Juan John-Charles <whoisju1@gmail.com>
2018-02-19 12:19:57 -0500

    create applications type

---
+++ schema/application_type.js
@@ -0,0 +1,65 @@
+const {
+  GraphQLObjectType,
+  GraphQLBoolean,
+  GraphQLString,
+  GraphQLID,
+} = require('graphql');
+
+// const User = mongoose.model('User');
+
+const ApplicationType = new GraphQLObjectType({
+  name: 'applications',
+  fields: {
+    _id: {
+      type: GraphQLID,
+    },
+    company: {
+      type: GraphQLString,
+    },
+    position: {
+      type: GraphQLString,
+    },
+    companyPhone: {
+      type: GraphQLString,
+    },
+    companyEmail: {
+      type: GraphQLString,
+    },
+    companyLocation: {
+      type: GraphQLString,
+    },
+    companyWebsite: {
+      type: GraphQLString,
+    },
+    companyInfo: {
+      type: GraphQLString,
+    },
+    jobID: {
+      type: GraphQLString,
+    },
+    status: {
+      type: GraphQLString,
+    },
+    favorite: {
+      type: GraphQLBoolean,
+    },
+    compensation: {
+      type: GraphQLString,
+    },
+    jobDetails: {
+      type: GraphQLString,
+    },
+    source: {
+      type: GraphQLString,
+    },
+    dateCreated: {
+      type: GraphQLString,
+    },
+    dateApplied: {
+      type: GraphQLString,
+    },
+  },
+});
+
+module.exports = ApplicationType;
+
